f131e4798c1e6afbfc8f5e60cf06b86584ad5c4e
Hans Dembinski <hans.dembinski@gmail.com>
2020-10-23 01:37:11 +0200

    u

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <utility>
 #include <vector>
 #include <cassert>
-#include <bitset>
 #include <random>
-#include <unordered_map>
 #include <cmath>
+#include <sstream>
+#include <memory>
+#include <cstdint>
+#include <stdexcept>
 #include <iostream>
-#include <iomanip>
+#include <algorithm>
 
 namespace u {
 constexpr float m = 1.f;
@@ -20,14 +22,13 @@ constexpr float g = 1e-3 * kg;
 }
 
 using rng_t = std::default_random_engine; // should be switched to PCG
-rng_t global_rng(1); // some seed
 
 constexpr auto kinf = std::numeric_limits<float>::infinity();
 constexpr auto knan = std::numeric_limits<float>::quiet_NaN();
 constexpr auto keps = std::numeric_limits<float>::epsilon();
 
 struct ParticleId {
-    enum {
+    enum Kind {
         Photon = 0,
         Electron = -1,
         Positron = 1,
@@ -42,7 +43,12 @@ ParticleId abs(ParticleId i) {
 }
 
 struct Particle : ParticleId {
-    float x, ct, p; // x = position, ct = time * speed of light, p = momentum
+    float x, p; // x = position, p = momentum
+
+    Particle() = default;
+    Particle(short i, float xx, float pp) :
+      ParticleId{i}, x{xx}, p{pp}
+    {}
 };
 
 template <class T>
@@ -91,12 +97,10 @@ inline int charge(const ParticleId& p) {
     return p.pid > 0 ? 1 : -1;
 }
 
-struct ProcessBase;
-
 // Particle entry with metadata needed for propagation
 struct StackParticle : Particle {
     float step = kinf; // current step size
-    ProcessBase* handler = nullptr; // winning process that handles this step
+    std::uint8_t pidx = 0; // index of process that handles this step
 
     StackParticle() = default;
     StackParticle(const Particle& p) : Particle{p} {}
@@ -116,12 +120,15 @@ struct StackRange {
 };
 
 struct ProcessBase {
+    std::uint8_t pidx_ = 0;
+
     using todo_t = std::vector<StackParticle*>;
     todo_t todo_;
     void handle(StackParticle& p) { todo_.push_back(&p); }
 
-    virtual void step(StackRange r) = 0;
+    virtual void step(StackRange) = 0;
     virtual void run(Stack&) = 0;
+    virtual ~ProcessBase() {}
 
     void run_and_reset(Stack& s) {
         run(s);
@@ -145,10 +152,8 @@ struct Move : ProcessBase {
         // optimization opportunity not implemented here:
         // we can run SIMD code to compute next N particles
         for (auto&& p : r) {
-            if (p.step > 0) {
-                p.step = std::min(max_step_, obs_level_ - p.x) + keps;
-                p.handler = this;
-            }
+            p.pidx = 0;
+            p.step = std::min(max_step_, obs_level_ - p.x) + keps;
         }
     };
 
@@ -158,7 +163,6 @@ struct Move : ProcessBase {
         for (auto p : ProcessBase::todo_) {
             if (p->step > 0) {
                 p->x += p->step;
-                p->ct += p->step / beta(*p);
                 if (charge(*p) != 0) {
                     const float de_dx = 2.127E-03 * u::MeV / u::cm; // Nitrogen 1 atm
                     const float eloss = de_dx * p->step;
@@ -183,15 +187,17 @@ struct Move : ProcessBase {
 template <class T>
 struct DecayOrInteraction : ProcessBase {
     using exp_dist = std::exponential_distribution<float>;
+
+    std::shared_ptr<rng_t> rng_;
     exp_dist dist_;
+
+    DecayOrInteraction(std::shared_ptr<rng_t> rng) : rng_{rng} {}
 
     float sample_length(const Particle& p) {
         const auto dl = static_cast<T*>(this)->length(p);
         if (dl == kinf)
             return kinf;
-        const auto l = dist_(global_rng) * dl;
-        std::cout << "dl = " << dl <<  " l = " << l << std::endl;
-        return l;
+        return dist_(*rng_) * dl;
     }
 
     virtual void step(StackRange r) override {
@@ -201,7 +207,7 @@ struct DecayOrInteraction : ProcessBase {
             const auto l = sample_length(p);
             if (l < p.step) {
                 p.step = l;
-                p.handler = this;
+                p.pidx = this->pidx_;
             }
         }
     };
@@ -209,6 +215,8 @@ struct DecayOrInteraction : ProcessBase {
 
 // decay process with static decay table, alternative could load table from file
 struct Decay : DecayOrInteraction<Decay> {
+
+    using DecayOrInteraction<Decay>::DecayOrInteraction;
 
     float length(const Particle& p) const {
         if (abs(p.pid) == ParticleId::Muon) {
@@ -230,6 +238,8 @@ struct Decay : DecayOrInteraction<Decay> {
 // demo interaction process
 struct PairProduction : DecayOrInteraction<PairProduction> {
 
+    using DecayOrInteraction<PairProduction>::DecayOrInteraction;
+
     float length(const Particle& p) const {
         if (p.pid == ParticleId::Photon) {
             return  3.260E+04 * u::cm; // Nitrogen, 1 atm
@@ -241,7 +251,7 @@ struct PairProduction : DecayOrInteraction<PairProduction> {
         for (auto* p : this->todo_) {
             // convert photon already on stack to electron
             p->pid = ParticleId::Electron;
-            p->p = std::sqrt(p->p * p->p / 2 - mass(*p));
+            p->p = std::sqrt(sqr(p->p) / 4 - sqr(mass(*p)));
 
             // add other electron to stack
             Particle p2{*p};
@@ -251,39 +261,184 @@ struct PairProduction : DecayOrInteraction<PairProduction> {
     }
 };
 
-int main() {
-    // initial stack
-    Stack stack = { Particle{ParticleId::Photon, 0.f, 0.f, 1 * u::GeV } };
+struct Bremsstrahlung : DecayOrInteraction<Bremsstrahlung> {
 
-    Move m(1 * u::m, 3 * u::MeV, 10 * u::km );
-    Decay d;
-    PairProduction pp;
+    using DecayOrInteraction<Bremsstrahlung>::DecayOrInteraction;
 
-    std::vector<ProcessBase*> processes = { &m, &d, &pp };
-
-    for (int i = 0; i < 3; ++i) {
-        // compute all step sizes
-        for (auto&& p : processes)
-            p->step(StackRange(stack.begin(), stack.end()));
-
-        // attach particles to winning process with shortest step size
-        for (auto&& p : stack) {
-            if (p.handler) {
-                // all particles need to be moved
-                if (p.handler != &m)
-                    m.handle(p);
-                p.handler->handle(p);
-            }
+    float length(const Particle& p) const {
+        if (abs(p.pid) == ParticleId::Positron) {
+            return  3.260E+04 * u::cm; // Nitrogen, 1 atm
         }
+        return kinf;
+    }
 
-        // run processes and fill stack with new particles
-        for (auto&& p : processes)
-            p->run_and_reset(stack);
-
-        std::cout << "iteration " << i << " energy deposit " << m.energy_deposit_ << std::endl;
-        for (auto&& p : stack) {
-            std::cout << "  pid " << std::setw(2) << p.pid << " x/m " << p.x/u::m << " ct/m "
-                      << p.ct/u::m << " p/GeV " << p.p / u::GeV << std::endl;
+    virtual void run(Stack& s) override {
+        for (auto* p : this->todo_) {
+            // apply eloss to electron/positron already on stack
+            p->p /= 2;
+            // add photon to stack
+            Particle p2{*p};
+            p2.pid = ParticleId::Photon;
+            s.push_back(p2);
         }
     }
+};
+
+struct ProcessList {
+  std::vector<std::shared_ptr<ProcessBase>> procs_;
+
+  bool run(Stack& s) {
+    // finished particles are at front
+    auto b = s.begin();
+    for (auto e = s.end(); b != e && b->step < 0; ++b);
+    if (b == s.end())
+      return false;
+
+    StackRange r(b, s.end());
+
+    // compute all step sizes
+    for (auto&& p : procs_)
+        p->step(r);
+
+    for (auto&& p : s) {
+        // all particles need to be moved; Move is at front
+        procs_.front()->handle(p);
+        if (p.pidx)
+          // attach particles to process with shortest step size
+          procs_[p.pidx]->handle(p);
+    }
+
+    // run processes and fill stack with new particles
+    for (auto&& p : procs_)
+        p->run_and_reset(s);
+
+    // move finished to front
+    std::remove_if(s.begin(), s.end(), [](StackParticle& p) {
+      return p.step > 0;
+    });
+
+    // delete particles below energy threshold
+    auto end = std::remove_if(s.begin(), s.end(), [](StackParticle& p) {
+      return p.p == 0;
+    });
+    s.resize(end - s.begin());
+
+    return true;
+  }
+
+  void append(std::shared_ptr<ProcessBase> p) {
+    if (procs_.empty()) {
+      if (!dynamic_cast<Move*>(p.get()))
+        throw std::runtime_error("Move must be first");
+    }
+    p->pidx_ = procs_.size();
+    procs_.push_back(p);
+  }
+};
+
+std::ostream& operator<<(std::ostream& os, const Particle& p) {
+  os << "Particle(" << p.pid << ", " << p.x << ", " << p.p << ")";
+  return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const StackParticle& p) {
+  os << "StackParticle(" << p.pid << ", " << p.x << ", " << p.p << ", " << p.step << ", " << static_cast<int>(p.pidx) << ")";
+  return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Stack& s) {
+  os << "[";
+  for (auto&& p : s)
+    os << "\n  " << p;
+  os << (s.empty() ? "]" : "\n]");
+  return os;
+}
+
+template <class T>
+std::string str(const T& t) {
+  std::ostringstream os;
+  os << t;
+  return os.str();
+}
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl_bind.h>
+
+namespace py = pybind11;
+
+PYBIND11_MAKE_OPAQUE(Stack);
+
+PYBIND11_MODULE(corsika, m) {
+
+  py::class_<rng_t, std::shared_ptr<rng_t>>(m, "RNG")
+    .def(py::init<unsigned long long>())
+    ;
+
+  py::enum_<ParticleId::Kind>(m, "ParticleId")
+    .value("Photon", ParticleId::Photon)
+    .value("Eletron", ParticleId::Electron)
+    .value("Positron", ParticleId::Positron)
+    .value("Muon", ParticleId::Muon)
+    .value("AntiMuon", ParticleId::AntiMuon)
+    .export_values()
+    ;
+
+  py::class_<Particle>(m, "Particle")
+    .def(py::init<short, float, float>())
+    .def_readwrite("pid", &Particle::pid)
+    .def_readwrite("x", &Particle::x)
+    .def_readwrite("p", &Particle::x)
+    .def_property_readonly("charge", [](Particle& p) { return charge(p); })
+    .def_property_readonly("mass", [](Particle& p) { return mass(p); })
+    .def_property_readonly("energy", [](Particle& p) { return energy(p); })
+    .def("__repr__", &str<Particle>)
+    ;
+
+  py::class_<StackParticle, Particle>(m, "StackParticle")
+    .def(py::init<Particle>())
+    .def_readwrite("step", &StackParticle::step)
+    .def_property_readonly("pidx", [](StackParticle& p )->int { return p.pidx; })
+    .def("__repr__", &str<StackParticle>)
+    ;
+
+  py::implicitly_convertible<Particle, StackParticle>();
+
+  py::bind_vector<Stack>(m, "Stack")
+    .def("__str__", &str<Stack>)
+    ;
+
+  py::class_<ProcessList>(m, "ProcessList")
+    .def(py::init<>())
+    .def("append", [](ProcessList& p, py::object o) {
+      p.append(py::cast<std::shared_ptr<ProcessBase>>(o));
+    })
+    .def("run", &ProcessList::run)
+    .def("__getitem__", [](ProcessList& pl, int i) { return pl.procs_[i]; })
+    .def("__len__", [](const ProcessList& pl) { return pl.procs_.size(); })
+    ;
+
+  py::class_<ProcessBase, std::shared_ptr<ProcessBase>>(m, "ProcessBase");
+
+  py::class_<Move, std::shared_ptr<Move>, ProcessBase>(m, "Move")
+    .def(py::init<float, float, float>())
+    .def_readwrite("energy_deposit", &Move::energy_deposit_)
+    ;
+
+  py::class_<Decay, std::shared_ptr<Decay>, ProcessBase>(m, "Decay")
+    .def(py::init<std::shared_ptr<rng_t>>())
+    .def("length", &Decay::length)
+    .def("sample_length", &Decay::sample_length)
+    ;
+
+  py::class_<PairProduction, std::shared_ptr<PairProduction>, ProcessBase>(m, "PairProduction")
+    .def(py::init<std::shared_ptr<rng_t>>())
+    .def("length", &PairProduction::length)
+    .def("sample_length", &PairProduction::sample_length)
+    ;
+
+  py::class_<Bremsstrahlung, std::shared_ptr<Bremsstrahlung>, ProcessBase>(m, "Bremsstrahlung")
+    .def(py::init<std::shared_ptr<rng_t>>())
+    .def("length", &Bremsstrahlung::length)
+    .def("sample_length", &Bremsstrahlung::sample_length)
+    ;
 }
---
+++ run.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+import subprocess as subp
+import sys
+
+p = Path("main.cpp")
+for p2 in Path().glob("corsika*.so"):
+    if p2.stat().st_mtime > p.stat().st_mtime:
+        break
+else:
+    subp.check_call("make")
+
+from corsika import (
+    ProcessList,
+    Move,
+    Decay,
+    PairProduction,
+    Bremsstrahlung,
+    Stack,
+    Particle,
+    ParticleId,
+    RNG,
+)
+
+if len(sys.argv) == 2:
+    seed = int(sys.argv[1])
+else:
+    seed = 12345678
+
+rng = RNG(seed)
+
+stack = Stack()
+
+p = Particle(ParticleId.Photon, 0, 10)
+
+stack.append(p)
+
+pl = ProcessList()
+m = Move(100, 1e-3, 2e3)
+pl.append(m)
+pl.append(Decay(rng))
+pl.append(PairProduction(rng))
+pl.append(Bremsstrahlung(rng))
+
+n = 0
+while pl.run(stack):
+    n += 1
+    print("iteration", n, "energy deposit", m.energy_deposit)
+    for p in stack:
+        print("  ", p)
+esum = m.energy_deposit
+for p in stack:
+    esum += p.energy
+print(f"energy {esum}")
